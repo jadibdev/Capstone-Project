8f610383587679eb41db81c7c89eb7cf84617ba9
Mohamed Jadib <jadibdev@gmail.com>
2022-05-19 12:38:19 +0100

    Run linters & Update index.html, style.css and main.js

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# google-io
+![Project Screenshot](https://user-images.githubusercontent.com/6723417/169280665-763e0664-953c-4496-b271-6461f80b00c9.png)
+
+This is a capstone project from my week 5 at Microverse, it is a website for Google I/O conference 2022, it features a dropdwon mobile menu, a hero section, a programs section, speakers section and a partners section. I have provided a live link below, looking forward to your feedback :-)
+
+## Built With
+
+- HTML
+- CSS
+- JAVASCRIPT
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps:
+
+### Install
+
+Download this repository to your personal computer
+
+### Usage
+
+Run the index.html file in your favorite browser
+
+## Authors
+
+👤 **Mohamed Jadib**
+
+- Portfolio: [@MJ](https://jadibdev.github.io/)
+- GitHub: [@jadibdev](https://github.com/jadibdev)
+- Twitter: [@MohamedJadib](https://twitter.com/MohamedJadib)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/mohamed-jadib-942a5041/)
+
+👤 **Ritobroto Mukherjee**
+
+- Portfolio: [@Rito](https://ritobrotomukherjee.github.io/Work-Portfolio/)
+- GitHub: [@RitobrotoMukherjee](https://github.com/RitobrotoMukherjee)
+- Twitter: [@RitobrotoM3](https://twitter.com/RitobrotoM3)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/ritobroto-mukherjee-519148ba/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse Community
+- Cindy Shin on Behance
+- Fontawesome.com
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- javascript/main.js
+++ javascript/main.js
@@ -1,133 +1,48 @@
-/* const getProjectListParentDiv = document.getElementById('');
-const projectList = [
-  {
-    name: 'Sundar Pichai',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featured_image: 'nature-35.png',
-    technologies: ['html-label.png', 'css-label.png', 'javascript-label.png'],
-    live: '#',
-    source: '#',
-  },
-  {
-    name: 'Multi-Post Stories',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featured_image: 'multi-post-stories2.png',
-    technologies: ['html-label.png', 'css-label.png', 'javascript-label.png'],
-    live: '#',
-    source: '#',
-  },
-  {
-    name: 'Facebook 360',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featured_image: 'Snapshoot-Portfolio.png',
-    technologies: ['html-label.png', 'css-label.png', 'javascript-label.png'],
-    live: '#',
-    source: '#',
-  },
-  {
-    name: 'Uber Navigation',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featured_image: 'multi-post-stories.png',
-    technologies: ['html-label.png', 'css-label.png', 'javascript-label.png'],
-    live: '#',
-    source: '#',
-  },
-
-];
-
-const createMainElements = (elem, classes) => {
-  const mainElem = document.createElement(elem);
-  if (classes !== '') {
-    const classesList = classes.split(' ');
-    mainElem.classList.add(...classesList);
-  }
-  return mainElem;
-};
-
-for (let index = 0; index < projectList.length; index += 1) {
-  const mainDiv = createMainElements('div', 'speakers-container container d-flex flex-column');
-  const speakerCard = createMainElements('div', 'project-subtitle-container');
-
-  const image = createMainElements('img', 'speaker-pic');
-  image.src = projectList[index].featured_image;
-  image.alt = `Project ${index + 1} image`;
-
-  const h2 = createMainElements('h2', 'speaker-name');
-  h2.textContent = projectList[index].name;
-
-  const speakerProfession = createMainElements('div', 'speaker-profession');
-  speakerProfession.textContent = 'CEO of alpahbet & google';
-
-  const projectDescDiv = createMainElements('div', 'project-description');
-  const projectDesc = createMainElements('p', '');
-  projectDesc.textContent = projectList[index].description;
-  projectDescDiv.appendChild(projectDesc);
-
-  const subtitleChildright = createMainElements('div', 'project-subtitle-right');
-
-  speakerCard.appendChild(speakerProfession);
-  speakerCard.appendChild(subtitleChildright);
-
-  mainDiv.appendChild(image);
-  speakerCard.appendChild(h2);
-  // mainDiv.appendChild(speakerCard);
-  speakerCard.appendChild(projectDescDiv);
-  
-  getProjectListParentDiv.append(mainDiv);
-  getProjectListParentDiv.append(speakerCard);
-}
- */
-
 const template = `
-  <h1 class="text-center mb-5 mt-3">Featured Speakers</h1>
+  <h1 class="text-center mt-3">Featured Speakers</h1>
+   <p class="text-center dash">____<p>
     <div class="container speakers-container">
       <div class="row">
         <div class="col-md-12 col-lg-6 speaker-card">
           <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
             <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h6>CEO of google & alphabet</h6>
           </div>
         </div>
         <div class="col-md-12 col-lg-6 speaker-card">
-          <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
+          <img src="https://io.google/2022/speakers/a3332795-c791-4949-9b9a-caf37a59011c_500.webp" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
-            <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h2>Josh Woodward</h2>
+            <h6>Senior Director @ Google Labs</h6>
           </div>>
         </div>
         <div class="col-md-12 col-lg-6 speaker-card">
-          <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
+          <img src="https://io.google/2022/speakers/583513af-02ad-4d43-a33a-a0a0bcf3b8b2_500.webp" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
-            <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h2>Prabhakar Raghavan</h2>
+            <h6>SVP of search</h6>
           </div>
         </div>
         <div class="col-md-12 col-lg-6 speaker-card">
-          <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
+          <img src="https://io.google/2022/speakers/7190d67c-53d5-4d20-9459-d37e7210e8ef_500.webp" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
-            <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h2>Sissie Hsiao</h2>
+            <h6>VP & GM, Assistant</h6>
           </div>>
         </div>
         <div class="col-md-12 col-lg-6 speaker-card">
-          <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
+          <img src="https://io.google/2022/speakers/c9fb48bf-575a-493f-be1f-db3c50727774_500.webp" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
-            <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h2>Trystan Upstill</h2>
+            <h6>VP, Engineering, Android</h6>
           </div>
         </div>
         <div class="col-md-12 col-lg-6 speaker-card">
-          <img src="/assets/images/sundar.jpeg" class="img-fluid" alt="sundar pichai">
+          <img src="https://io.google/2022/speakers/bfe09f6d-27e7-4b68-a93e-617e20d7fc31_500.webp" class="img-fluid" alt="sundar pichai">
           <div class="container speaker-decription">
-            <h2>Sundar Pichai</h2>
-            <h6>CEO of google & alpahbet</h6>
-            <p>joined google in 2012 after larry left ..</p>
+            <h2>Sameer Samat</h2>
+            <h6>VP, Product Management, Android</h6>
           </div>>
         </div>
       </div>
